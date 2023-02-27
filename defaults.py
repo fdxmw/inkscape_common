1be11c46d685d3c9a116a5f9c1e1fc5b8aaf4880
Jeremy Lau <30300826+fdxmw@users.noreply.github.com>
2023-02-26 20:12:00 -0800

    Refactor common default drawing settings to defaults.py

---
+++ defaults.py
@@ -0,0 +1,18 @@
+'''Various default drawing preferences for Inkscape extensions.'''
+
+defaults = {
+    # Default color for cut lines.
+    'cut_color': '#880000',
+    # Default color for crease lines.
+    'crease_color': '#008800',
+    # Default color for pen lines.
+    'pen_color': '#000088',
+
+    'stroke_width': '0.1mm',
+
+    # Gap between adjacent templates.
+    'template_spacing': '1mm',
+
+    # Default fill colors. There must be at least two.
+    'fill_colors': ['#ffe9a6', '#a8cbfe'],
+}
